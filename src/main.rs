47785700d95faaab8f4fe0562dfafb8ea984cca5
Ilango <rajagopalilango@gmail.com>
2023-04-19 22:13:32 +0530

    👌 IMPROVE: Use connection pooling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 #![allow(dead_code, unused_variables)] // TODO: remove when done
 
+use sqlx::PgPool;
 use std::net::TcpListener;
 use z2p::{config::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(&address)?;
     println!("Listening for connections at {address}");
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,9 @@
-use actix_web::web::Form;
+use actix_web::web::{Data, Form};
 use actix_web::HttpResponse;
+use chrono::Utc;
 use serde::Deserialize;
+use sqlx::{query, PgPool};
+use uuid::Uuid;
 
 #[allow(dead_code)]
 #[derive(Deserialize)]
@@ -9,6 +12,24 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(_form: Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn subscribe(form: Form<FormData>, connection: Data<PgPool>) -> HttpResponse {
+    match query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(connection.get_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {e}");
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
